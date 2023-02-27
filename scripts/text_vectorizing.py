9c410b63e4faba72037fe1b70c79ad284158f16d
Fido Aragon <fidoarg@gmail.com>
2023-02-27 10:45:26 -0300

    Functionality to TextVectorizer added for w2vec

--- scripts/text_vectorizing.py
+++ scripts/text_vectorizing.py
@@ -1,40 +1,61 @@
+from gensim.models import Word2Vec
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from pandas.core.frame import DataFrame
 import numpy as np
 
-from pandas import DataFrame
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.base import TransformerMixin, BaseEstimator
-
-
 class TextVectorizer(BaseEstimator, TransformerMixin):
-    def __init__(self, kind='bow'):
+    def __init__(self, kind='bow', w2vec=False):
         self.kind = kind
-        if kind == 'bow':
-            self.vectorizer = CountVectorizer()
-        elif kind == 'tfidf':
-            self.vectorizer = TfidfVectorizer()
+        self.w2vec = w2vec
+        if not w2vec:
+            if kind == 'bow':
+                self.vectorizer = CountVectorizer()
+            elif kind == 'tfidf':
+                self.vectorizer = TfidfVectorizer()
+            else:
+                raise ValueError('Unsupported vectorizer type')
         else:
-            raise ValueError('Unsupported vectorizer type')
+            self.w2v_model = None
 
     def fit(self, X, y=None):
+        if not self.w2vec:
+            if isinstance(X, DataFrame):
+                self.text_data = X.squeeze().values.astype(str)
+            elif isinstance(X, np.ndarray):
+                self.text_data = np.asarray(X).squeeze().astype(str)
+            else:
+                raise TypeError('Unsupported data type')
 
-        if isinstance(X, DataFrame):
-            self.text_data = X.squeeze().values.astype(str)
-        elif isinstance(X, np.ndarray):
-            self.text_data = np.asarray(X).squeeze().astype(str)
+            self.vectorizer.fit(self.text_data)
         else:
-            raise TypeError('Unsupported data type')
-
-        self.vectorizer.fit(self.text_data)
+            # Train a Word2Vec model on the input data.
+            sentences = [doc.split() for doc in X]
+            self.w2v_model = Word2Vec(sentences, size=100, window=5, min_count=5, workers=3)
 
         return self
 
     def transform(self, X, y=None):
+        if not self.w2vec:
+            if isinstance(X, DataFrame):
+                X = X.squeeze().values.astype(str)
+            elif isinstance(X, np.ndarray):
+                X = np.asarray(X).squeeze().astype(str)
+            else:
+                raise TypeError('Unsupported data type')
 
-        if isinstance(X, DataFrame):
-            X = X.squeeze().values.astype(str)
-        elif isinstance(X, np.ndarray):
-            X = np.asarray(X).squeeze().astype(str)
+            return self.vectorizer.transform(X)
         else:
-            raise TypeError('Unsupported data type')
-
-        return self.vectorizer.transform(X)
+            # Convert each document to a vector by averaging its word vectors.
+            X_vectors = []
+            for doc in X:
+                doc_vector = []
+                for word in doc.split():
+                    if word in self.w2v_model.wv.vocab:
+                        doc_vector.append(self.w2v_model.wv[word])
+                if doc_vector:
+                    doc_vector = np.mean(doc_vector, axis=0)
+                    X_vectors.append(doc_vector)
+                else:
+                    X_vectors.append(np.zeros(100))
+            return np.array(X_vectors)
